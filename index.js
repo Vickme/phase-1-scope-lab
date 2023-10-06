4bff2a66bd34b5989222f24f755aafaf2e83d8e6
victor <victor.njutha@student.moringaschool.com>
2023-10-06 15:22:51 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+// Declare a variable in global scope
+var customerName;
+
+// Function to uppercase the global customerName variable
+function upperCaseCustomerName() {
+  if (customerName) {
+    customerName = customerName.UpperCase();
+  } else {
+    console.log();
+  }
+}
+
+// Function to set a variable in global scope
+function setBestCustomer() {
+  bestCustomer = "not bob"; 
+}
+
+// Function to overwrite a variable in global scope
+function overwriteBestCustomer() {
+  bestCustomer = "still not bob"; 
+}
+
+// Declare a constant in global scope
+const leastFavoriteCustomer = "Mark";
+
+// Function attempting to change a constant
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "Bob";
+  // It will result in an error 
+}
+
